f366c7bf8d3a28c4fe639726b5cd3eb15a02d3e3
playpizza <pzzaq@naver.com>
2022-04-01 09:16:42 +0900

    update board urls and views
    board의 urls와 views 기본구조 생성

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from . import views
 
 app_name = "Board"
 
 urlpatterns = [
+    path("board/list/", views.board_list, name="list"),
+    path("board/write/", views.board_write, name="write"),
+    path("board/detail/<int:id>/", views.board_detail, name="detail"),
+    path("board/update/<int:pk>/", views.board_update, name="update"),
+    path("board/delete/", views.board_delete, name="delete"),
+    path("board/comment/", views.board_comment, name="comment"),
+    path("board/per_page/", views.board_per_page, name="per_page"), 
 ]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def board_list(request):
+    # TODO
+    pass
+
+
+def board_write(request):
+    # TODO
+    pass
+
+
+def board_detail(request, id):
+    # TODO
+    pass
+
+
+def board_update(request, id):
+    # TODO
+    pass
+
+
+def board_delete(request):
+    # TODO
+    pass
+
+
+def board_comment(request): # 댓글달기
+    # TODO
+    pass
+
+
+def board_per_page(request):
+    # TODO
+    pass
