e09f1a2d100fe2a0db57fa93e2306d7433260b2b
playpizza <pzzaq@naver.com>
2022-04-01 09:19:49 +0900

    modify path
    board path의 불필요한 path 제거

--- board/urls.py
+++ board/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "Board"
 
 urlpatterns = [
-    path("board/list/", views.board_list, name="list"),
-    path("board/write/", views.board_write, name="write"),
-    path("board/detail/<int:id>/", views.board_detail, name="detail"),
-    path("board/update/<int:pk>/", views.board_update, name="update"),
-    path("board/delete/", views.board_delete, name="delete"),
-    path("board/comment/", views.board_comment, name="comment"),
-    path("board/per_page/", views.board_per_page, name="per_page"), 
+    path("list/", views.board_list, name="list"),
+    path("write/", views.board_write, name="write"),
+    path("detail/<int:id>/", views.board_detail, name="detail"),
+    path("update/<int:pk>/", views.board_update, name="update"),
+    path("delete/", views.board_delete, name="delete"),
+    path("comment/", views.board_comment, name="comment"),
+    path("per_page/", views.board_per_page, name="per_page"), 
 ]
--- contents/urls.py
+++ contents/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = "Contents"
 
 urlpatterns = [
+    path("list/", views.contents_list, name="list"),
 ]
