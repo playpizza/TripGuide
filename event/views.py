70be8b965ec44bd3aa5f3f4bdbe95df5a500f787
playpizza <pzzaq@naver.com>
2022-04-05 17:33:10 +0900

    update event_detail.html
    이벤트 상세보기 코드 작성

--- event/views.py
+++ event/views.py
@@ -1,15 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import EventBoard, EventDocument
 # from django.core.paginator import Paginator
 
 def event_list(request):
     all_events = EventBoard.objects.all().order_by('-id')
-
-
-
     return render(request, 'event_list.html', {'events': all_events})
 
 
 
 def event_detail(request, id):
-    return render(request, 'event_detail.html')
+    try:
+        event = EventBoard.objects.get(id=id)
+    except EventBoard.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다.')
+    return render(request, 'event_detail.html', {'event': event})
