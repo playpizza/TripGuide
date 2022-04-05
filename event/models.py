14ebc8189d0179e53a7db856bb5af03062f7613e
playpizza <pzzaq@naver.com>
2022-04-05 12:38:40 +0900

    update add\models.py, event\models.py
    db설계에 따른 models코드 추가

--- event/models.py
+++ event/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    # 제목
+    title = models.CharField(max_length=128)
+    # 내용
+    contents = models.TextField()
+    # 조회수
+    view_cnt = models.IntegerField(default=0)
+    # 작성일
+    reg_date = models.DateTimeField(auto_now_add=True)
+    # 만료일
+    exp_date = models.DateField()
+
+    def __str__(self):
+        return self.title
