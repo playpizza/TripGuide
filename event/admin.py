bd5b260f9d550cc26fb0c42a97f7c167e2023b71
playpizza <pzzaq@naver.com>
2022-04-05 16:08:15 +0900

    update event\list
    데이터 개수만큼 이벤트 표시되도록 코드 작성

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import EventBoard
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    
+admin.site.register(EventBoard, EventAdmin)
--- event/views.py
+++ event/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import EventBoard, EventDocument
+# from django.core.paginator import Paginator
 
 def event_list(request):
-    # TODO
-    pass
+    all_events = EventBoard.objects.all().order_by('-id')
+
+
+
+    return render(request, 'event_list.html', {'events': all_events})
+
 
 
 def event_detail(request, id):
-    # TODO
-    pass
+    return render(request, 'event_detail.html')
