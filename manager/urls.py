d6eb86a5395a810378ee28195215db254f5d5d3b
playpizza <pzzaq@naver.com>
2022-04-06 16:59:58 +0900

    update manager
    urls.py, views.py에 기본적인 url과 함수 뼈대 작성
    manage_base.html 기본형 작성, temp.html작성

--- manager/urls.py
+++ manager/urls.py
@@ -5,4 +5,21 @@ app_name = "Manager"
 
 urlpatterns = [
     path("home/", views.manage_home, name="home"), 
+    path("user/stats/", views.m_user_stats, name="userStats"), 
+    path("user/manage/", views.m_user_manage, name="userManage"), 
+    path("user/detail/<int:id>/", views.m_user_detail, name="userDetail"),
+    path("contents/stats/", views.m_contents_stats, name="contentsStats"), 
+    path("contents/manage/", views.m_contents_manage, name="contentsManage"), 
+    path("board/stats/", views.m_board_stats, name="boardStats"), 
+    path("board/manage/", views.m_board_manage, name="boardManage"), 
+    path("review/stats/", views.m_review_stats, name="reviewStats"), 
+    path("review/manage/", views.m_review_manage, name="reviewManage"), 
+    path("comment/stats/", views.m_comment_stats, name="commentStats"), 
+    path("comment/manage/", views.m_comment_manage, name="commentManage"), 
+    path("event/stats/", views.m_event_stats, name="eventStats"), 
+    path("event/detail/<int:id>/", views.m_event_detail, name="eventDetail"), 
+    path("ad/stats/", views.m_ad_stats, name="adStats"), 
+    path("ad/detail/<int:id>/", views.m_ad_detail, name="adDetail"), 
+    path("question/stats/", views.m_question_stats, name="questionStats"), 
+    path("question/detail/<int:id>/", views.m_question_detail, name="questionDetail"), 
 ]
--- manager/views.py
+++ manager/views.py
@@ -4,3 +4,89 @@ from django.shortcuts import render
 def manage_home(request):
     # TODO
     pass
+
+
+def m_user_stats(request):
+    # TODO
+    pass
+
+
+def m_user_manage(request):
+    # TODO
+    pass
+
+
+def m_user_detail(request, id):
+    # TODO
+    pass
+
+
+def m_contents_stats(request):
+    # TODO
+    pass
+
+
+def m_contents_manage(request):
+    # TODO
+    pass
+
+
+def m_board_stats(request):
+    # TODO
+    pass
+
+
+def m_board_manage(request):
+    # TODO
+    pass
+
+
+def m_review_stats(request):
+    # TODO
+    pass
+
+
+def m_review_manage(request):
+    # TODO
+    pass
+
+
+def m_comment_stats(request):
+    # TODO
+    pass
+
+
+def m_comment_manage(request):
+    # TODO
+    pass
+
+
+def m_event_stats(request):
+    # TODO
+    pass
+
+
+def m_event_detail(request, id):
+    # TODO
+    pass
+
+
+def m_ad_stats(request):
+    # TODO
+    pass
+
+
+def m_ad_detail(request, id):
+    # TODO
+    pass
+
+
+def m_question_stats(request):
+    # TODO
+    pass
+
+
+def m_question_detail(request, id):
+    # TODO
+    pass
+
