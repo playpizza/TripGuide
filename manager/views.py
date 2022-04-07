13f18aba2ac6453b3ab316f5cb288000b1a74e53
playpizza <pzzaq@naver.com>
2022-04-07 17:26:47 +0900

    update manager view.py
    관리자 home에서 CountBoard에서 데이터를 가져와서 표시하도록 코드 작성

--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,44 @@
+from datetime import datetime
 from django.shortcuts import render
+from count.models import CountBoard
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def manage_home(request):
+    context = {
+        'today_login': 0,
+        'user_num': 0,
+        'today_contents_view': 0,
+        'activation_contents': 0,
+        'today_board': 0,
+        'report_board': 0,
+        'today_review': 0,
+        'report_review': 0,
+        'today_comment': 0,
+        'report_comment': 0,
+        'on_event': 0,
+        'today_event_veiw': 0,
+        'today_question': 0,
+        'undo_question': 0,
+        'on_ad': 0,
+        'today_ad_view':0, 
+    }
+    try:
+        today_count = CountBoard.objects.get(reg_date=datetime.today())
+        context['today_login'] = today_count.login_cnt
+        context['today_contents_view'] = today_count.trip_view_cnt + today_count.food_view_cnt + today_count.lodging_view_cnt + today_count.festival_view_cnt
+        context['today_board'] = today_count.board_cnt
+        context['today_review'] = today_count.review_cnt
+        context['today_comment'] = today_count.comment_cnt
+        context['today_event_veiw'] = today_count.event_view_cnt
+        context['today_ad_view'] = today_count.ad_view_cnt
 
-    return render(request, 'm_home.html')
+    except ObjectDoesNotExist:
+        pass
+
+
+
+    return render(request, 'm_home.html', context)
 
 
 def m_user_stats(request):
