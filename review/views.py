4c0638b8b7c6315dbd43b39ded3314dcda1e4cc4
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-08 15:25:20 +0900

    Update views.py

--- review/views.py
+++ review/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import F_review, H_review, R_review, T_review
+from .forms import ReviewForm
+from member.models import User
 
-# Create your views here.
+def review_write(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)  
+        if form.is_valid():
+            user_id = request.session.get('user') 
+            user = User.objects.get(pk=user_id)
+
+            review = R_review()  
+            review.title = form.cleaned_data['title']
+            review.content = form.cleaned_data['contents']
+            review.writer = user
+            review.save() 
+           
+            return redirect('/review/list/')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'review_write.html', {'form': form})
+
+def review_list(request):
+    r_reviews = R_review.objects.all().order_by('-id') 
+    h_reviews = H_review.objects.all().order_by('-id') 
+    f_reviews = F_review.objects.all().order_by('-id') 
+    t_reviews = T_review.objects.all().order_by('-id') 
+
+    return render(request, 'review_list.html', {'r_reviews': r_reviews}, {'h_reviews': h_reviews}, {'f_reviews': f_reviews}, {'t_reviews': t_reviews})
+
+def review_detail(request, pk):  
+    r_review = R_review.objects.get(pk=pk)  
+    return render(request, 'review_detail.html', {'r_review': r_review})
