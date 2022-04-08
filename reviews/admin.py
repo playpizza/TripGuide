254383cae1b504aee8d6d490785da395008b18db
playpizza <pzzaq@naver.com>
2022-04-08 17:16:08 +0900

    review폴더 삭제, admin.py 코드 작성
    reviews폴더와 review폴더의 중복문제로 review폴더 삭제
    편의성을 위해 review와 board의 admin.py 코드 작성

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import R_review, H_review, F_review, T_review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    
+admin.site.register(R_review, ReviewAdmin)
+admin.site.register(H_review, ReviewAdmin)
+admin.site.register(F_review, ReviewAdmin)
+admin.site.register(T_review, ReviewAdmin)
