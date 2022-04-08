c8a61e62ca36033872e35228bf5ba5fbba5f9891
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-08 15:55:44 +0900

    Create urls.py

---
+++ reviews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("list/", views.review_list),
+    path("write/", views.review_write),
+    path("detail/<int:pk>/", views.review_detail),
+]
