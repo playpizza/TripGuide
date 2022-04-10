74a3cf9409a4425454dc53911bee615ccaa8d3ac
playpizza <pzzaq@naver.com>
2022-04-10 22:28:33 +0900

    update m_user_stats
    m_user_stats에서 views.py에서 넘긴 데이터가 js까지 전달되고 차트로 표시되도록 코드 수정
    
    html에서 views로 조건을 받아오도록 코드 작성
    
    db에서 데이터를 가져올 때 데이터가 없을 경우 작동할 코드 작성

--- manager/views.py
+++ manager/views.py
@@ -56,9 +56,6 @@ def manage_home(request):
         context['user_num'] = User.objects.all().count()
     except ObjectDoesNotExist:
         pass
-    
-
-
 
     return render(request, 'm_home.html', context)
 
@@ -66,14 +63,44 @@ def manage_home(request):
 def m_user_stats(request):
     context = {
         'today': datetime.today().strftime("%Y-%m-%d"),
-        'last_7days': (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d")
+        'last_7days': (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d"),
+        'labelData': [],
+        'numData': [],
+        # 통계값
+        'user_num': 0,
+        'ban_users': 0,
+        'today_login': 0,
     }
-    all_users = User.objects.all()
-    context['user_num'] = all_users.count()
-    context['today_login'] = CountBoard.objects.get(reg_date=datetime.today()).login_cnt
-    context['ban_users'] = all_users.filter(is_banned=1).count()
+    # 기본통계
+    try:
+        all_users = User.objects.all()
+        context['user_num'] = all_users.count()
+        context['ban_users'] = all_users.filter(is_banned=1).count()
+    except ObjectDoesNotExist:
+        pass
+    try:
+        context['today_login'] = CountBoard.objects.get(reg_date=datetime.today()).login_cnt
+    except ObjectDoesNotExist:
+        temp = CountBoard()
+        temp.save()
 
-    
+    # 차트 조건
+    if request.method == 'POST':
+        viewType = request.POST['view_type']
+        startDate = request.POST['start_date']
+        endDate = request.POST['end_date']
+        viewRange = request.POST['view_range']
+    else:
+        viewType = 'view_user'
+        startDate = context['last_7days']
+        endDate = context['today']
+        viewChartType = 'day'
+        
+    # 차트 데이터
+    context['labelData'] = [
+    ]
+    context['numData'] = [
+    ]
     
 
 
--- static/bs/js/manager/user_stats.js
+++ static/bs/js/manager/user_stats.js
@@ -1,55 +1,45 @@
 /* globals Chart:false, feather:false */
 
+var labelData = [];
+var numData = [];
+$(".labelData").each(function(i, item){labelData.push(item.innerHTML);});
+$(".numData").each(function(i, item){numData.push(item.innerHTML);});
+
 (function () {
-    'use strict'
-  
-    feather.replace({ 'aria-hidden': 'true' })
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturdayd',
-          'Saturdday',
-        ],
-        datasets: [{
-          data: [
-            159,
-            11345,
-            18483,
-            14003,
-            13489,
-            14092,
-            12034,
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
+  'use strict'
+
+  feather.replace({
+    'aria-hidden': 'true'
+  })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labelData,
+      datasets: [{
+        data: numData,
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
         }]
       },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
+      legend: {
+        display: false
       }
-    })
-  })()
-  
+    }
+  })
+})();
+
