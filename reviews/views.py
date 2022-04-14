def3a202032a947b25ece3e9a55d6670f68eefa3
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-15 06:46:43 +0900

    페이징

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .models import F_review, H_review, R_review, T_review
 from .forms import ReviewForm
 from member.models import User
+from django.core.paginator import Paginator
 
 def review_write(request):
     if request.method == "POST":
@@ -24,10 +25,14 @@ def review_write(request):
     return render(request, 'review_write.html', {'form': form})
 
 def review_list(request):
-    r_reviews = R_review.objects.all().order_by('-id') 
-    h_reviews = H_review.objects.all().order_by('-id') 
-    f_reviews = F_review.objects.all().order_by('-id') 
-    t_reviews = T_review.objects.all().order_by('-id') 
+    all_r_reviews = R_review.objects.all().order_by('-id') 
+    # h_reviews = H_review.objects.all().order_by('-id') 
+    # f_reviews = F_review.objects.all().order_by('-id') 
+    # t_reviews = T_review.objects.all().order_by('-id') 
+
+    page = int(request.GET.get('p', 1)) 
+    paginator =  Paginator (all_r_reviews, 5)  
+    r_reviews = paginator.get_page(page) 
 
     return render(request, 'review_list.html', {'r_reviews': r_reviews})
 
