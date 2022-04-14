1c44ac5e5c0d53d74dcdedd4734eb18b761b20c3
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-14 12:12:58 +0900

    Update views.py

--- contents/views.py
+++ contents/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+import urllib
+from django.shortcuts import  render
+from contents.models import Content
+import requests
 
+def get_contents(request):
+    all_contents = {}
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        keyword = urllib.parse.quote(keyword)
+        api_key = '6%2B5mrhfOlLaSxBRdUTJHl8Y7e7yURzgyJWjob%2BXgC%2BJgdjZCYrHb%2Fro6Fv61BA6ngjv6lzwoq8XCh660UfLdOQ%3D%3D'
 
-def contents_list(request):
-    # TODO
-    pass
+        url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=" + api_key + "&MobileApp=AppTest&MobileOS=ETC&pageNo=1&numOfRows=" + '10' + "&listYN=Y&arrange=A&contentTypeId=12&keyword=" + keyword + "&_type=json"
 
+        response = requests.get(url)
+        data = response.json()
+        contents = data['response']['body']['items']['item']
 
-def contents_detail(request, id):
-    # TODO
-    pass
+        for i in contents:
+            content_data = Content(
+                title = i['title'],
+                addr1 = i['addr1'],
+                firstimage = i['firstimage']
+            )
+            content_data.save()
+            all_contents = Content.objects.all().order_by('-id')
 
+    return render (request, 'content.html', { "all_contents": all_contents})
 
-def contents_write(request):
-    # TODO
-    pass
-
-
-def contents_delete(request):
-    # TODO
-    pass
-
+def content_detail(request, id):
+    content = Content.objects.get(id = id)
+    print(content)
+    return render (request, 'content_detail.html', {'content': content})
