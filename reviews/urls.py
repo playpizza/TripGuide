a54561d90b13d54fec702bc52839c34dca19d8f8
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-15 05:58:00 +0900

    link

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list/", views.review_list),
-    path("write/", views.review_write),
-    path("detail/<int:pk>/", views.review_detail),
+    path("list/", views.review_list, name="r_list"),
+    path("write/", views.review_write, name="r_write"),
+    path("detail/<int:pk>/", views.review_detail, name="r_detail"),
 ]
