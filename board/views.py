e8a72c76d96d2107246475062b948eb7f3117e93
JeongHyeon-E <102194817+JeongHyeon-E@users.noreply.github.com>
2022-04-14 17:04:06 +0900

    board

--- board/views.py
+++ board/views.py
@@ -68,6 +68,7 @@ class BoardListView(ListView):
 
         context['type'] = search_type
         context['ad'] = getAd()
+        context['now'] = 'board'
 
         return context
 
@@ -120,6 +121,7 @@ def board_detail(request, id):
             'board': board,
             'comments': comment_list,
             'ad': getAd(),
+            'now' : 'board'
         }
     except Board.DoesNotExist:
         raise Http404('게시글을 찾을수 없습니다')
