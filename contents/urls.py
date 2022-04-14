8dffe32f6a0039ab090a895913f4655867388f2e
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-14 12:22:00 +0900

    migration

--- contents/urls.py
+++ contents/urls.py
@@ -6,7 +6,7 @@ app_name = "Contents"
 
 urlpatterns = [
     path("list/", views.get_contents, name="get_contents"),
-    path("detail/<int:id>/", views.contents_detail, name="detail"),
+    path("detail/<int:id>/", views.content_detail, name="detail"),
     # path("review/write/", views.contents_write, name="review_write"),
     # path("review/delete/", views.contents_delete, name="review_delete"), 
 ]
