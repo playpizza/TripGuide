818c06cf123ee574915e3db8c61e09fccc972b9d
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-15 00:57:54 +0900

    Update views.py

--- reviews/views.py
+++ reviews/views.py
@@ -8,12 +8,13 @@ def review_write(request):
         form = ReviewForm(request.POST)  
         if form.is_valid():
             user_id = request.user
-            user = User.objects.get(pk=user_id)
+            # user = User.objects.get(pk=user_id)
 
             review = R_review()  
             review.title = form.cleaned_data['title']
             review.content = form.cleaned_data['content']
-            review.writer = user
+            review.grade = form.cleaned_data['grade']
+            review.writer = user_id
             review.save() 
            
             return redirect('/review/list/')
