393eccabac63d72aff430e6ab028040c6a4261ce
Hanjea07 <102266050+Hanjea07@users.noreply.github.com>
2022-04-14 12:14:30 +0900

    Update urls.py

--- contents/urls.py
+++ contents/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "Contents"
 
 urlpatterns = [
-    path("list/", views.contents_list, name="list"),
+    path("list/", views.get_contents, name="get_contents"),
     path("detail/<int:id>/", views.contents_detail, name="detail"),
-    path("review/write/", views.contents_write, name="review_write"),
-    path("review/delete/", views.contents_delete, name="review_delete"), 
+    # path("review/write/", views.contents_write, name="review_write"),
+    # path("review/delete/", views.contents_delete, name="review_delete"), 
 ]
